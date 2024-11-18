f136c7530a503d11ffa45a68a2fff0cb50cf34d8
Ahmaduddin Shaikh <44947415+saahilshaikh@users.noreply.github.com>
2024-11-18 13:24:37 +0530

    feat: event trigger message

--- src/routes/slack.js
+++ src/routes/slack.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import axios from 'axios';
 
 const router = Router();
 
@@ -13,7 +14,33 @@ router.post('/saveTeamInfo', (req, res) => {
 router.post('/event', (req, res) => {
   const { challenge } = req.body;
   console.log('Slack event: ', req.body);
+  sendMessage(process.env.DUMMY_MESSAGE, 'C07U3DW9J0J');
   return res.send({ challenge });
 });
+
+async function sendMessage(message, channel_id) {
+  let payload = {
+    text: message,
+    channel: channel_id,
+  };
+  const config = {
+    headers: {
+      Authorization: 'Bearer ' + process.env.PARAGON_TOKEN,
+    },
+  };
+
+  try {
+    const response = await axios.post(
+      `https://proxy.useparagon.com/projects/${process.env.PARAGON_PROJECT_ID}/sdk/proxy/slack/chat.postMessage`,
+      payload,
+      config,
+    );
+    console.log('Paragon response: ', response);
+    return { status: true, data: response.data };
+  } catch (error) {
+    console.log('Error sending message to Slack!');
+    return { status: false, error: error };
+  }
+}
 
 export default router;
