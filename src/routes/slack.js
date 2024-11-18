dc4707ba22bac86efafea82237dace44084d748a
Ahmaduddin Shaikh <44947415+saahilshaikh@users.noreply.github.com>
2024-11-18 12:06:59 +0530

    slack event listner

--- src/routes/slack.js
+++ src/routes/slack.js
@@ -1,12 +1,19 @@
-import { v4 as uuidv4 } from 'uuid';
 import { Router } from 'express';
 
 const router = Router();
 
 router.post('/saveTeamInfo', (req, res) => {
   const { team_id, org_id } = req.body;
-  console.log(team_id, org_id);
+  console.log(
+    `Slack send team info trigger: team_id: ${team_id}, org_id: ${org_id}`,
+  );
   return res.send({ team_id, org_id });
+});
+
+router.post('/event', (req, res) => {
+  const { challenge } = req.body;
+  console.log('Slack event: ', req.body);
+  return res.send({ challenge });
 });
 
 export default router;
