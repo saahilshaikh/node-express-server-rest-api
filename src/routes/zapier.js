17b073b2b33402fb40b1c5713fbec37b59712a7f
saahil-teammates <saahil@teammates.ai>
2025-06-24 13:00:40 +0530

    wip: zapier integration

--- src/routes/zapier.js
+++ src/routes/zapier.js
@@ -11,4 +11,13 @@ router.get('/test', (req, res) => {
     });
 });
 
+router.post('/create_job', (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    console.log(JSON.stringify(req.body));
+    res.status(200).json({
+        status: 'success',
+        timestamp: new Date().toISOString()
+    });
+});
+
 export default router;
