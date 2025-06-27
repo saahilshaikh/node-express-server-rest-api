47569c9c4892f862ee8865995bcbbd5fd72dc34f
saahil-teammates <saahil@teammates.ai>
2025-06-27 22:44:12 +0530

    wip: zapier

--- src/routes/zapier.js
+++ src/routes/zapier.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { Router } from 'express';
 
 const router = Router();
@@ -18,6 +19,17 @@ router.post('/create_job', (req, res) => {
         status: 'success',
         timestamp: new Date().toISOString()
     });
+});
+
+router.get('/get_org_jobs', (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    axios.get(`https://api.dev.workforce.uktob.ai/v1/jobs?org_id=223c3ae0-9905-4968-8e85-28fd046790f5&page=1&limit=100`)
+        .then(response => {
+            res.status(200).json(response.data);
+        })
+        .catch(error => {
+            res.status(error.status).json({ error: 'Failed to fetch stages' });
+        });
 });
 
 export default router;
