a3074e2fd9333a2a273293ec7971a1db791418d5
saahil-teammates <saahil@teammates.ai>
2025-06-30 12:21:55 +0530

    Update zapier.js

--- src/routes/zapier.js
+++ src/routes/zapier.js
@@ -37,4 +37,13 @@ router.get('/get_org_jobs', (req, res) => {
         });
 });
 
+router.post('/create_job_linked_interview', (req, res) => {
+    console.log(JSON.stringify(req.headers));
+    console.log(JSON.stringify(req.body));
+    res.status(200).json({
+        status: 'success',
+        timestamp: new Date().toISOString()
+    });
+});
+
 export default router;
